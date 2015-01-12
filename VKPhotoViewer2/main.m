ac9da84640d1289f2b0b4a924c69fd06218bf32b
Lana-Resquit <kristy@Mac-Pro-stillHitch.local>
2015-01-12 13:30:30 -0800

    Initial Commit

---
+++ VKPhotoViewer2/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VKPhotoViewer2
+//
+//  Created by Kristy on 12.01.15.
+//  Copyright (c) 2015 Kristyna. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
